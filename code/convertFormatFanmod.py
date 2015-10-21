c91aa15e19a6a3cbf0973832e8f44d1f235dadd4
Ubuntu <ubuntu@ip-172-31-26-243.ap-southeast-1.compute.internal>
2015-10-21 10:02:59 +0000

    analyze data in aws. may modify code a bit

--- code/convertFormatFanmod.py
+++ code/convertFormatFanmod.py
@@ -3,8 +3,8 @@ from igraph import *
 ## Create edgelist-formated files for fanmod programs
 
 def convert():#F6F67B#F6F67B
-	oldpath = "/home/amm/Desktop/sna-project/sna-git/data/"
-	newpath = "/home/amm/Desktop/sna-project/shared_windows/motif/edgelist/"
+	oldpath = "/home/ubuntu/Desktop/sna_utcc/data/gml/notempnode/"
+	newpath = "/home/ubuntu/Desktop/sna_utcc/data/gml/motif_edgelist/"
 	error_list = []
 	#fname_list = ["ICT56_friend.gml","ICT56_bf.gml","ICT56_study.gml"]
 	for fname in os.listdir(oldpath):
@@ -20,7 +20,7 @@ def convert():#F6F67B#F6F67B
 			for e in g.es():
 				src = int(g.vs[e.source]["id"])
 				dest = int(g.vs[e.target]["id"])
-				if src in (9999, 99999) or dest in (9999, 99999): continue
+				#if src in (9999, 99999) or dest in (9999, 99999): continue
 				line = str(src)+" "+str(dest)+"\n"
 				f_w.write(line)
 			f_w.close()
--- code/motif_analysis.py
+++ code/motif_analysis.py
@@ -1,7 +1,7 @@
 import os
 import igraph as ig
 
-def myplot(result_path, fname, g, size):
+def plotMotif(result_path, fname, g, size):
 	
 	f_r = open(result_path+fname,"r")
 	
@@ -33,39 +33,15 @@ def myplot(result_path, fname, g, size):
 		print gname
 		ig.plot(subg, plotpath+gname )  #
 		
-		
-		
-	'''	
-	num_group = len(group_arr)
-	color_dict = {0:"blue", 1:"green", 2:"red", 3:"cyan", 4:"magenta", 5:"yellow", 6:"black" }
-	i = 0
-	print "num groups = "+str(num_group)
-	for group in group_arr:
-		
-		if len(group) == 1:
-			for node in group:
-				g.vs[node]["color"] = "white"
- 		else:
-			print "group "+str(i)
-			for node in group:
-				
-				g.vs[node]["color"] = color_dict[i]
-			i+=1
-	gname = "/home/amm/Desktop/sna-git/result/"+fname.replace(".gml","")+"_"+gtype+".png"
-	plot(g,gname)   '''
+ 
+	
 	
 def main(): 
-	result_path = "/home/amm/Desktop/sna-project/shared_windows/motif/results/"
-	gml_path = "/home/amm/Desktop/sna-project/sna-git/data/gml/"
-	size = 3 
-	fname_list = ["ICT56_friend.gml","ICT56_bf.gml","ICT56_study.gml"]
-	for fname in os.listdir(result_path):
-		
-		if fname.endswith(".csv.dump"):
-			print fname
-			gname = fname.strip(".csv.dump")+".gml"
-  			g = ig.read(gml_path+gname,  format="gml").simplify()
-			myplot(result_path, fname, g, size)
-			 
+	result_path = "/home/ubuntu/Desktop/sna_utcc/results/analysis/"
+	gml_path = "/home/ubuntu/Desktop/sna_utcc/data/gml/notempnode/"
+	size = 2 
+ 
+	for fname in os.listdir(gml_path):
+		 
 			
 main()
