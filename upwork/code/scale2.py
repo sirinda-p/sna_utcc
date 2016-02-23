9c98dcf03026414ada09742f6beee27240d39783
Ubuntu <ubuntu@ip-172-31-26-243.ap-southeast-1.compute.internal>
2016-02-23 16:40:57 +0000

    rm unneccesary dir

--- upwork/code/scale2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn import preprocessing
-
-
-X_train = np.array([[ 1., -1.,  2.] ]).transpose()
- 
-min_max_scaler = preprocessing.MinMaxScaler()
-X_train_minmax = min_max_scaler.fit_transform(X_train)
-print X_train_minmax.transpose()
